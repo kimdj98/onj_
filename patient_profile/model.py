537d6f444d7cc5046e1169a9342a22170378a63a
Ironsesameoil <127085535+Ironsesameoil@users.noreply.github.com>
2024-04-24 18:01:12 +0900

    Create model.py

---
+++ patient_profile/model.py
@@ -0,0 +1,92 @@
+import pandas as pd
+from sklearn.utils import shuffle
+from sklearn import svm
+from sklearn.metrics import confusion_matrix
+from matplotlib import pyplot as plt
+from sklearn.metrics import classification_report
+from sklearn.metrics import precision_score, recall_score, accuracy_score, roc_auc_score
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+
+
+## read and convert
+#data_x = pd.read_csv("/mnt/4TB1/onj/dataset/v0/X.csv")
+#data_y = pd.read_csv("/mnt/4TB1/onj/dataset/v0/Y.csv")
+
+data_x = pd.read_csv('D:\Work\보건복지부과제\ONJ\onj\X.csv') 
+data_y = pd.read_csv('D:\Work\보건복지부과제\ONJ\onj\Y.csv')
+
+
+
+len_data_x = len(data_x)
+len_data_y = len(data_y)
+
+
+train_x, test_x, train_y, test_y = train_test_split(data_x, data_y, test_size=0.2, shuffle=False, random_state=0)
+
+train_x = train_x.drop(train_x.columns[0],axis=1)
+test_x = test_x.drop(test_x.columns[0],axis=1)
+train_y = train_y.drop(train_y.columns[0],axis=1)
+test_y = test_y.drop(test_y.columns[0],axis=1)
+
+
+train_y = train_y.squeeze()
+test_y = test_y.squeeze()
+
+
+
+####### train ########
+classifier = svm.SVC(kernel='linear', C=2.2)
+
+classifier.fit(train_x, train_y)
+
+supportVectors = classifier.support_vectors_
+supportVectorIndices = classifier.support_
+supportVectorCount = classifier.n_support_
+
+
+
+####### test ########
+
+testPrediction = classifier.predict(test_x)
+
+
+confusionMatrix = confusion_matrix(test_y, testPrediction)
+print("Confusion Matrix: \n", confusionMatrix)
+
+
+print("Accuracy of the Classifier: %.2f" %(100*accuracy_score(test_y, testPrediction)),"%")
+
+
+print("Precision value of the Positive Class ('Normal' Class): %.2f" %(100*precision_score(test_y, testPrediction,  average="binary")), "%")
+print("Recall value of the Positive Class ('Normal' Class): %.2f" %(100*recall_score(test_y, testPrediction,  average="binary")), "%")
+
+
+#Classification Report for each class:
+target_names = ['Normal', 'Abnormal']
+print("Precision and Recall for each class: \n",classification_report(test_y, testPrediction, target_names=target_names))
+
+print("Support Vectors Count: \n", supportVectorCount)
+print(supportVectorCount[0]+supportVectorCount[1])
+
+print("area under curve (auc): ", roc_auc_score(test_y, testPrediction))
+
+
+
+####### visualize Confusion Matrix ########
+import seaborn as sns
+fig, ax= plt.subplots(figsize=(8,6))
+
+
+sns.heatmap(confusionMatrix, annot=True, linewidths=.5, xticklabels=True, yticklabels=True)
+ax.xaxis.set_ticklabels(['Normal', 'Abnormal'], fontsize=12); ax.yaxis.set_ticklabels(['Normal', 'Abnormal'], fontsize=12)
+
+ax.set_title('Confusion Matrix', fontsize=20)
+ax.set_xlabel('Predicted Labels', fontsize=14)
+ax.set_ylabel('True Labels', fontsize=14)
+
+
+plt.show()
+
+
