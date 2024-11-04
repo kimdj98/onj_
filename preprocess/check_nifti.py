0815f82c85830d377008d0d4901f1d782cbd6f4e
kimdj98 <kimdj98g@gmail.com>
2024-11-04 10:54:56 +0900

    change onj folder name

--- preprocess/check_nifti.py
+++ preprocess/check_nifti.py
@@ -8,6 +8,7 @@ from pathlib import Path
 error_folders = []
 CT_MODALS = ["MDCT", "CBCT"]
 
+
 def check_niftis(data_path):
     for patient in list(data_path.glob("*")):
         for modal in list(patient.glob("*")):
@@ -18,7 +19,8 @@ def check_niftis(data_path):
                         # check if output.nii.gz exists in nifti folder
                         if "nifti" in os.listdir(modal):
                             if "output.nii.gz" in os.listdir(modal / "nifti"):
-                                print("Checked", modal / "nifti" / "output.nii.gz")
+                                # print("Checked", modal / "nifti" / "output.nii.gz")
+                                pass
                             else:
                                 print("Error in checking", modal / "nifti" / "output.nii.gz")
                                 error_folders.append(modal)
@@ -26,19 +28,21 @@ def check_niftis(data_path):
                         print("Error in checking", modal)
                         continue
 
+
 @hydra.main(version_base="1.3", config_path="../config", config_name="config")
 def main(cfg: DictConfig):
     DATAPATH = Path(cfg.data.data_dir)
 
     # NOTE: switch below two code lines to check ONJ or Non_ONJ_soi
-    MODAL_PATH = DATAPATH / "ONJ_labeling"
+    MODAL_PATH = DATAPATH / "onj"
     check_niftis(MODAL_PATH)
 
-    MODAL_PATH = DATAPATH / "Non_ONJ_soi"
+    MODAL_PATH = DATAPATH / "non_onj"
     check_niftis(MODAL_PATH)
+
 
 if __name__ == "__main__":
     main()
-    
+
     for f in error_folders:
         print(f)
